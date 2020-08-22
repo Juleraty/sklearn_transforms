972fe4980e81cf18694e6c7cda9dc92bfb01ca8d
Juleraty <58960763+Juleraty@users.noreply.github.com>
2020-08-22 00:18:15 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,24 @@ from sklearn import tree
 from sklearn.metrics import accuracy_score
 import numpy as np
 
+iris  =  load_iris ()
 
+X  =  íris . dados
+y  =  íris . alvo 
+
+
+y_names  =  iris . target_names
+
+
+test_ids  =  np . aleatório . permutação ( len ( X ))
+
+
+
+X_train  =  X [ test_ids [: - 100 ]]
+X_test  =  X [ test_ids [ - 100 :]]
+
+y_train  =  y [ test_ids [: - 100 ]]
+y_test  =  y [ test_ids [ - 100 :]]
 
 setup(
       name='my_custom_sklearn_transforms',
