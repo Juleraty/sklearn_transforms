048eb5b2af2f233085da8c32da2659e5f0ee21c5
Juleraty <58960763+Juleraty@users.noreply.github.com>
2020-08-22 00:04:08 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,6 +1,23 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
+iris = load_iris()
 
+X = iris.data
+y = iris.target 
+
+
+y_names = iris.target_names
+
+
+test_ids = np.random.permutation(len(X))
+
+
+
+X_train = X[test_ids[:-100]]
+X_test = X[test_ids[-100:]]
+
+y_train = y[test_ids[:-100]]
+y_test = y[test_ids[-100:]]
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
